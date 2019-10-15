3827e6cf8ed70c64db28c07360627502f3d5ca21
Jasonccs <1521910992@qq.com>
2019-10-15 11:32:16 +0800

    update

--- initial/src/main/java/com/hello/HelloController.java
+++ initial/src/main/java/com/hello/HelloController.java
@@ -3,10 +3,9 @@ package com.hello;
 import com.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,12 +15,25 @@ public class HelloController {
 
     @Autowired
     HelloService helloService;
-    
-    @RequestMapping("/test")
+
+    @GetMapping("/test")
     @ResponseBody
-    public List index() {
-         List<Map> list = helloService.selectUserList();
-         return list;
+    public Map<String,Object> index(@RequestParam int page, @RequestParam int limit) {
+
+
+        List<Map> list = helloService.selectUserList();
+
+
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("total", limit);
+        map1.put("pages", page);
+        map1.put("list", list);
+
+        map.put("code", 0);
+        map.put("msg", "百度");
+        map.put("data", map1);
+        return map;
     }
-    
+
 }
